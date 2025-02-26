97af0404647ec2beba46c7bc4746927849272726
arizfaiyaz <arizfaiyazwork@gmail.com>
2025-02-27 00:02:19 +0530

    fix: resolve bcrypt import issue and enhance user signup functionality

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -2,6 +2,7 @@ import {connect} from "@/dbconfig/dbConfig";
 import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
+import bcryptjs from "bcryptjs";
 
 
 connect();
@@ -19,9 +20,23 @@ export async function POST(request: NextRequest) {
        }
 
        // hash password
+       const salt = await bcrypt.genSalt(10);
+       const hashedPassword = await bcryptjs.hash(password, salt);
+
+       const newUser = new User ({
+        username,
+        email,
+        password: hashedPassword
+       })
+       const savedUser = await newUser.save();
+       console.log(savedUser);
+
+       return NextResponse.json({
+        message: "User created successfully",
+        success: true,
+        savedUser
+    })
        
-
-
     } catch (error: any) {
         return NextResponse.json({error: error.message},
         {status: 500});
